8cf6c8163ea4150959543ff542116c383a5dec31
Jesus Arias <jesus.d.arias96@gmail.com>
2017-08-19 17:52:43 -0600

    Cambio server.js

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+var http = require('http');
+
+function onRequest(request, response) {
+    response.writeHead(200, {
+        'Content-type': 'text-plain'
+    });
+    response.write('Hello from Node.');
+    response.end();
+}
+
+// provess.env.PORT will expand to the name pipe value on App Service
+// request --> Frontends (ARR) --> Web Worker (IIS) --> iisnode --> 
+//    --named-pipe--> node.exe server.js
+
+http.createServer(onRequest).listen(process.env.PORT || 3000);
+console.log('Listening for requests on port ' + (process.env.PORT || 3000));
+
+/*
+
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -134,3 +153,5 @@ app.get('/clientes', function(request, response) {
 app.get('/pedidos', function(request, response) {
   response.render('pedido');
 });
+
+*/
