f2ab7b31f7bb535c012c91b8e81c32a3436c8559
Jesus Arias <jesus.d.arias96@gmail.com>
2017-08-19 18:00:44 -0600

    Cambio estructura servidor

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-var http = require('http');
-
-function onRequest(request, response) {
-    response.writeHead(200, {
-        'Content-type': 'text-plain'
-    });
-    response.write('Hello from Node.');
-    response.end();
-}
-
-// provess.env.PORT will expand to the name pipe value on App Service
-// request --> Frontends (ARR) --> Web Worker (IIS) --> iisnode --> 
-//    --named-pipe--> node.exe server.js
-
-http.createServer(onRequest).listen(process.env.PORT || 3000);
-console.log('Listening for requests on port ' + (process.env.PORT || 3000));
-
-/*
-
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -33,9 +14,10 @@ app.set('views', path.join(__dirname, 'public/views'));
 app.use(express.static(__dirname));
 
 //Listener
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('Server started on Port 3000');
 });
+
 
 //Body Parser Middleware
 app.use(bodyParser.json());
@@ -154,4 +136,3 @@ app.get('/pedidos', function(request, response) {
   response.render('pedido');
 });
 
-*/
